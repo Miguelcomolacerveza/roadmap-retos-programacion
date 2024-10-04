ba48fdf8a26308f305ffabce9c500bfbba8caeab
Miguelcomolacerveza <113274338+Miguelcomolacerveza@users.noreply.github.com>
2024-10-04 18:17:02 +0200

    #[00] - [python]

---
+++ Roadmap/00 - SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO/python/miguelcomolacerveza.py
@@ -0,0 +1,35 @@
+# Sitio web oficial de Python: https://www.python.org
+# Este es un comentario en una línea
+
+"""
+Este es un comentario
+que ocupa varias líneas
+y es genial para dejar notas más largas.
+"""
+
+# Variable que puede cambiar 
+nombre_banda = "Python Rockers"
+
+# Constante (por convención, la usamos en mayúsculas)
+ALBUM_LANZAMIENTO = "Python Rocks the World"
+
+# Cadenas de texto (strings)
+letra_cancion = "Python es puro rock and roll"
+
+# Entero (int)
+guitarras = 6
+
+# Flotante (float)
+precision_sonido = 99.99
+
+# Booleano (bool)
+este_codigo_es_un_exito = True
+
+# NoneType (sin valor, como una guitarra rota en el escenario)
+guitarra_ausente = None
+
+# Número complejo 
+sonido_futurista = 5 + 3j
+
+# Imprimir el gran saludo al lenguaje
+print("¡Hola, Python!")
